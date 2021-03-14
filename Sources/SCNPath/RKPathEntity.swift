8ae11197409cc6bf758d4965edac36f35ecaa1a3
Grant Jarvis <grantjarvis8@gmail.com>
2021-03-13 23:18:45 -0600

    RealityKit Implementation Option #2: Generate the path with circles and rectangles instead of saving a .usdz file to disk.

--- Sources/SCNPath/RKPathEntity.swift
+++ Sources/SCNPath/RKPathEntity.swift
@@ -22,18 +22,112 @@ public class RealityKitPathEntity : SCNPathNode {
     ///This is used for changing the file path each time.
     private var entityCount = 0
     
+    private var circleEntity : Entity?
+    
+    private var rectangleEntity : Entity?
+    
+    private var pathEntitiesInScene = [Entity]()
+    
+    override init(
+        path: [SCNVector3], width: Float = 0.5,
+        curvePoints: Float = 8, materials: [SCNMaterial] = []
+    ) {
+        super.init(path: path,
+                   width: width,
+                   curvePoints: curvePoints,
+                   materials: materials)
+        do {
+            let shapesScene = try ShapesProj.loadShapesScene()
+            loadShapes(scene: shapesScene)
+        } catch {
+            print(error.localizedDescription)
+            return
+        }
+        //Remove synchronization to save memory.
+        self.realityKitPath.visit(using: {$0.synchronization = nil})
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    private func loadShapes(scene: ShapesProj.ShapesScene){
+        
+        //Change pivot point.
+        rectangleEntity = Entity()
+        rectangleEntity?.addChild(scene.rectangle!)
+        
+        circleEntity = scene.circle
+        
+        //Then we can use custom materials (even with transparency) and video materials.
+        //We could also smooth the points ourselves. They are all on the 2D X-Z plane.
+    }
     
     override func recalcGeometry() {
-        super.recalcGeometry()
+        guard realityKitPath.isAnchored,
+              circleEntity != nil,
+              rectangleEntity != nil
+        else {return}
+        
+        //Start off with just a circle for the first position,
+        //Then every position after that gets a rectangle and a circle.
+        //This way, every rectangle has a circle on both ends.
+        if self.path.count == 1 {
+            let circleLocal = circleEntity!.clone(recursive: true)
+            realityKitPath.anchor!.addChild(circleLocal)
+            circleLocal.setPosition(self.path[0].getSIMD3Float(), relativeTo: nil)
+            pathEntitiesInScene.append(circleLocal)
+            return
+        }
+        
+        //Using a smaller distance between points on a curve will lead to smoother curves.
+        
+        //Keep all y-values the same so that the edges of the circles don't stick out.
+        let currentIndex = (path.count - 1)
+        let newPosition : SIMD3<Float> = [path[currentIndex].getSIMD3Float().x,
+                           path[0].y,
+                           path[currentIndex].getSIMD3Float().z]
+        let lastPosition : SIMD3<Float> = [path[currentIndex - 1].getSIMD3Float().x,
+                            path[0].y,
+                            path[currentIndex - 1].getSIMD3Float().z]
+        let length = simd_length(newPosition - lastPosition)
+        let rectangleLocal = rectangleEntity!.clone(recursive: true)
+        realityKitPath.anchor!.addChild(rectangleLocal)
+        pathEntitiesInScene.append(rectangleLocal)
+        rectangleLocal.position = lastPosition
+        rectangleLocal.position.y = newPosition.y
+
+        //The rectangle is already 1 meter long, so settings its scale in the Z-Dimension with the difference between the points (in meters) will leave it at the correct length, which is the distance between the points.
+        rectangleLocal.setScale([1, 1, length], relativeTo: nil)
+        //Rotate the rectangle to connect the dots.
+        // --(lastPosition is already at one end of the rectangle, rotate to put newPosition at the other end).
+        rectangleLocal.look(at: newPosition, from: lastPosition, relativeTo: realityKitPath.anchor!)
+        
+        //Put a circle at the tip of the rectangle.
+        let circleLocal = circleEntity!.clone(recursive: true)
+        realityKitPath.anchor!.addChild(circleLocal)
+        circleLocal.setPosition(newPosition, relativeTo: nil)
+        pathEntitiesInScene.append(circleLocal)
+        
+
+    }
+        
+        
+        
+        
+   // override func recalcGeometry() {
+        //super.recalcGeometry()
 
         //if we didn't use a different file path each time, the model would Not change.
-        makeRKGeo(path: "rkPath\(entityCount).usdz")
-        entityCount += 1
+        //makeRKGeo(path: "rkPath\(entityCount).usdz")
+        //entityCount += 1
         
         //Delete the old usdz files we saved to the device.
         //let appDelegate = UIApplication.shared.delegate as! AppDelegate
         //appDelegate.deleteTemporaryDirectory()
-    }
+    //}
     
     
     ///Make a .usdz file from the SceneKit scene that includes the SceneKit path so that we can use it in RealitKit.
@@ -77,7 +171,13 @@ public class RealityKitPathEntity : SCNPathNode {
 }
 
 
-
+extension SCNVector3 {
+    func getSIMD3Float() -> SIMD3<Float>{
+        return SIMD3<Float>([self.x,
+                             self.y,
+                             self.z])
+    }
+}
 
 
 
@@ -103,4 +203,13 @@ extension Entity {
                     return result
                 }}}
         return nil //default
-    }}
+    }
+    
+    func visit(using block: (Entity) -> Void) {
+        block(self)
+
+        for child in children {
+            child.visit(using: block)
+        }
+    }
+}
