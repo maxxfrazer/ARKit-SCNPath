5c1c290c44ff1694b7babfa3214e981f85c8340b
Max Cobb <maxxc@mac.com>
2021-03-15 11:00:57 +0000

    updated swiftlint, changed way the entity is added to the scene, bumped minimum ios version allowed to 13

--- Example/SCNPath+Example/RealityKit Version/RKViewController.swift
+++ Example/SCNPath+Example/RealityKit Version/RKViewController.swift
@@ -9,7 +9,7 @@
 import ARKit
 import FocusEntity
 import RealityKit
-//import SCNPath
+// import SCNPath
 
 class RKViewController: UIViewController{
 
@@ -20,20 +20,19 @@ class RKViewController: UIViewController{
     public let scnScene = SCNScene()
     public var focusEntity : FocusEntity!
 
-	var hitPoints = [SCNVector3]() {
-		didSet {
-			self.pathNode.path = self.hitPoints
-		}
-	}
+    var hitPoints = [SCNVector3]() {
+        didSet {
+            self.pathNode.path = self.hitPoints
+        }
+    }
 
-	var pathNode = RKPathEntity(path: [])
+    var pathNode = RKPathEntity(path: [])
 
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-        //If this device is running iOS 13.4 or later and has LiDAR, then allow occlusion with the LiDAR mesh.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // If this device is running iOS 13.4 or later and has LiDAR, then allow occlusion with the LiDAR mesh.
         if #available(iOS 13.4, *),
-            ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+           ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
                 runLiDARConfiguration()
         } else{
             runNonLiDARConfig()
@@ -42,68 +41,61 @@ class RKViewController: UIViewController{
         pathNode.scene = scnScene
         self.arView.session.delegate = self
 
-		self.arView.frame = self.view.bounds
-		self.arView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.arView.frame = self.view.bounds
+        self.arView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 
-		self.view.addSubview(arView)
+        self.view.addSubview(arView)
         
         let worldAnchor = AnchorEntity() //point 0,0,0
         self.arView.scene.addAnchor(worldAnchor)
         worldAnchor.addChild(pathNode.realityKitPath)
 
 
-		// the next chunk of lines are just things I've added to make the path look nicer
-		let pathMat = SCNMaterial()
+        // the next chunk of lines are just things I've added to make the path look nicer
+        let pathMat = SCNMaterial()
         
         //necessary for conversion to RealityKit usdz.
         pathMat.lightingModel = .physicallyBased
         
-		self.pathNode.materials = [pathMat]
-		self.pathNode.position.y += 0.05
-		if Int.random(in: 0...1) == 0 {
-			pathMat.diffuse.contents = UIImage(named: "path_seamless")
-			self.pathNode.textureRepeats = true
-		} else {
-			pathMat.diffuse.contents = UIImage(named: "path_with_fade")
-		}
-		self.pathNode.width = 0.5
+        self.pathNode.materials = [pathMat]
+        self.pathNode.position.y += 0.05
+        if Int.random(in: 0...1) == 0 {
+            pathMat.diffuse.contents = UIImage(named: "path_seamless")
+            self.pathNode.textureRepeats = true
+        } else {
+            pathMat.diffuse.contents = UIImage(named: "path_with_fade")
+        }
+        self.pathNode.width = 0.5
 
-		self.scnScene.rootNode.addChildNode(self.pathNode)
-		self.setupGestures()
+        self.scnScene.rootNode.addChildNode(self.pathNode)
+        self.setupGestures()
         
         do {
-          let onColor: MaterialColorParameter = try .texture(.load(named: "Add"))
-          let offColor: MaterialColorParameter = try .texture(.load(named: "Open"))
-          self.focusEntity = FocusEntity(
-            on: self.arView,
-            style: .colored(
-              onColor: onColor, offColor: offColor,
-              nonTrackingColor: offColor
+            let onColor: MaterialColorParameter = try .texture(.load(named: "Add"))
+            let offColor: MaterialColorParameter = try .texture(.load(named: "Open"))
+            self.focusEntity = FocusEntity(
+                on: self.arView,
+                style: .colored(
+                    onColor: onColor, offColor: offColor,
+                    nonTrackingColor: offColor
+                )
             )
-          )
         } catch {
             self.focusEntity = FocusEntity(on: self.arView, focus: .classic)
-          print("Unable to load plane textures")
-          print(error.localizedDescription)
+            print("Unable to load plane textures")
+            print(error.localizedDescription)
         }
-	}
-    
+    }
 
     func runNonLiDARConfig(){
-
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = [.horizontal, .vertical]
 
         arView.session.run(configuration)
     }
 
-
-	override func viewWillDisappear(_ animated: Bool) {
-		super.viewWillDisappear(animated)
-		arView.session.pause()
-	}
-    
-    
-    
-    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arView.session.pause()
+    }
 }
--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "SCNPath",
-  platforms: [.iOS("9.3")],
+  platforms: [.iOS(.v13)],
   products: [
     .library(
       name: "SCNPath",
